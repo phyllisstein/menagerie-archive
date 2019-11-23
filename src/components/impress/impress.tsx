c0b24619026beec59b0c8f54912df2763b4f4db2
Daniel P. Shannon <daniel@fleet.i.ng>
2019-11-22 21:49:34 -0500

    Initial commit.

---
+++ src/components/impress/impress.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Impress: React.FunctionComponent = ({ children }) => {
+  return (
+    <div className='impress-layout'>
+      { children }
+    </div>
+  )
+}
