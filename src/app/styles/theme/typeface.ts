52127928d26a67a790d5a5fe4d4779d5be5e1a8e
Daniel Shannon <daniel@daniel.sh>
2019-12-04 08:18:17 -0500

    :bento: Assets and theme.

---
+++ src/app/styles/theme/typeface.ts
@@ -0,0 +1,5 @@
+export const primary = ['FreightMacro Pro', 'Garamond', 'Times New Roman', 'Times', 'serif'].join(',')
+
+export const accent = ['FreightSans Pro', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Helvetica', 'Arial', 'sans-serif'].join(',')
+
+export const mono = ['Attribute Mono', 'SFMono-Regular', 'Consolas', 'Liberation Mono', 'Menlo', 'monospace'].join(',')
