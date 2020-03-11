499139eb5d3b0850785fd0cee6c2f7b8552bfdcd
Daniel P. Shannon <daniel@fleet.i.ng>
2020-03-11 08:21:27 -0400

    :building_construction: Refactor top-level app.

---
+++ config/banner.js
@@ -0,0 +1,67 @@
+/* cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+** ccccccccccccccccccccccccccccccccccccccccccccllllcccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+** cccccccccccccccccccccccccccccccccccccccdkOKXXXXXK0OkxoccccodxkkOOOOOOOkkxocccccccccccccccccccccccccccccccccccccccccccccc
+** cccccccccccccccccccccccccccccccccccclkKX0KKKKKKKKKKKXXXK0XNXK00KKKKKKKKKKKX0Oocccccccccccccccccccccccccccccccccccccccccc
+** ccccccccccccccccccccccccccccccccccckXK00K00KKKKKKKKKKKKKK00000000KK00000KKK00X0lcccccccccccccccccccccccccccccccccccccccc
+** ccccccccccccccccccccccccccccccccclKXK000KKK000KKKXKKK0000KKK0000000KKKK00000000Xoccccccccccccccccccccccccccccccccccccccc
+** cccccccccccccccccccccccccccccccccOKK0000KXXXXKK00KKKKXXXX000KK00000K000KK0K000K00ccccccccccccccccccccccccccccccccccccccc
+** cccccccccccccccccccccccccccccccdk0KKK0XXXK00000000KKKK00KNX0K0000000KK000K0K0000Kklccccccccccccccccccccccccccccccccccccc
+** ccccccccccccccccccccccccccccdOKK0K00KK0KK0000KKKKKKK0KXK0KXX0KKKKKKK00000000XKKKKKX0xcccccccccccccccccccccccccccllcccccc
+** ccccccccccccccccccccccccccoKK000000K000K000KKKKKKKKKXKKKK0KKX0000KKKKKKKKK0000K00KKKKKdcccccccccccccccccccccccclooolcccc
+** cccccccccccccccccccccccccxX00K0000Xxk00000KK0KNXKXWNNXKXX00KXKKKKKK00K00K000KKKKK00KX0K0occllollccccccccccccccloooooollc
+** cccccccccccccccccccccccccX00X0KKKXl.,OK00K00KKXK0XKXKXXKX00KXNKKOl,.d0K0KKK000000KKK0X00Xxooooooollccccccccccloooooooool
+** cccccccccccccccccccccccccX0KKK00Xc...'o0XK00K00KKKKKKXXKX0K0N:,'....k00XK00KKKKKKKKKK0K00Xxoooooooollccccccclooooooooooo
+** ccccccccccccccccllcccccckN00X00Xc.......,codkOOkxo:;;,:XKKK0O.......:X00KXXXXXKK00000KK0K0Kooooooooooollcclooooooooooooo
+** ccccccccccccccclooollccxX000X0Xo.......................K0XKKc.oNk....lXK0KKKKXXXKK00000K0KXdoooooooooooolloooooooooooooo
+** cccccccccccccloooooooolK0K00K0X;,......................OKKXKOKXXK.....'d0XK000KKKKK0KK000KXdoooooooooooooooooooooooooooo
+** ccccccccccccloooooooooo00KXXXXXXX0kxxd:...........l0K0XKXXXNXXXXXKk,.....'OXNXXXXK000KK0K0Xoooooooooooooooooooooollloooo
+** lcccccccccclooooooooooodNNXKKKKKXXXKKKO...........:KKKXKxllcllodk0Kk......O0KKKKKKKK00000Xkoooooooooooooooooooollccclloo
+** oollcccccloooooooooooookXXXXXNoc:::::,.............,c:'...................x0KKK000000000XXolooooooooooooooooollccccccccl
+** oooollccloooooooooooooooXK00KO,....................';;,;;;;;::c'..........XX00KXKKK0000000Klcllooooooooooooolccccccccccc
+** ooooooolooooooooooooooookX0K0::,..,:............'c:,........'',ox'.......lNNKKKKKKKKKK00000Occcclloooooooolccccccccccccc
+** ooooooooooooooooooooooodkko:;,,;:llx,..........lc.,,,:c:KNo......l.......XKKXK00KKK00000KX0Xdccccccllooolccccccccccccccc
+** ooooooooooooooooooooookx...,;:::;;''l.........ll..   :ddkWc      o......dXKK00000KKxodkK00X0Occcccccccllcccccccccccccccc
+** ooooooooooooooooooolclO'....o,xW...,d.........o.      .;'       'd.....,X0K0000XKc......xK00Kccccccccccccccccccccccccccc
+** ooooooooooooooooollcccx,    'l;.   .k..........d,             .cc......,X0000XXc.llclo'..K00Kccccccccccccccccccccccccccc
+** oooooooooooooooolccccccxd;.      .:Ko...........:dc:;'...',;:c:.........,odxd;..O,....x.'X00Kccccccccccccccccccccccccccc
+** lloooooooooooollccccccccco0dlcccddlO..............',;:c:,'.....................lx:;'..O.kK0Xkccccccccccccccccccccccccccc
+** ccclloooooooolccccccccccccld......O'...........................................,x'...dloX00Xlccccccccccccccccccccccccccc
+** ccccccllloollccccccccccccccO.....k,........;::co;..............................cd;.;dlkK0KXocccccccccccccccccccccccccccc
+** cccccccccclccccccccccccccclO.....x.............,c;;...............................'cxK0KXOlccccccccccccccccccccccccccccc
+** ccccccccccccccccccccccccccxo.....co.....,locdo.....,;;;,,,'......................OK00KXklccccccccccccccccccccccccccccccc
+** ccccccccccccccccccccccccccO;......'olcll::,.....;;:',l:.........................'XKKKXlccccccccccccccccccccccccccccccccc
+** ccccccccccccccccccccccccccO..............,::;c;'  ,,:;d......................0lclOlolccccccccccccccccccccccccccccccccccc
+** cccccccccccccccccccccccccck..........;l,';..  ;',;;l.o;.....................oc...Occcccccccccccccccccccccccccccccccccccc
+** cccccccccccccccccccccccccck.........xMMl.:,'''c  .,ll......................;x...odcccccccccccccccccccccccccccccccccccccc
+** cccccccccccccccccccccccccck,.........oOo,l;,,,c;:c........................;d....0ccccccccccccccccccccccccccccccccccccccc
+** ccccccccccccccccccccccccccod..........':...;::;;c,.............................xdccccccccccccccccccccccccccccccccccccccc
+** ccccccccccccccccccccccccccckc...........:;;...................................:Xdccccccccccccccccccccccccccccccccccccccc
+** ccccccccccccccccccccccccccccko'.........',;;;,'...............................x ;kcccccccccccccccccccccccccccccccccccccc
+** cccccccccccccccccccccccccccccdkc'.....'''...',;;..............................Oo,'xccccccccccccccccclllllccccccccccccccc
+** cccccccccccccccccccccccccccccccdko;l'...............co;...................;lcc.  .Klcccccccccccccclloooooollccccccccccll
+** cccccccccccccccccccccccccccccccccokdlOOdlc:;;;;coxKK;................,ccc:.      ;XKOlccccccccccllooooooooooollccccclloo
+** cccccccccccccccccccccccccccccccccclkk,dKMMMMMMMMNOc............;cc::;'           dK0KXdccccccclloooooooooooooooolllloooo
+** ccccccccccccccccccccccccccccclxdoc;.,x'.':ccc:;.........:c:::;,.        .        O0OOON0kocclloooooooooooooooooooooooooo
+** cccccccccccccccccccccccccccxKl. .....:c..............:olx.                      :X0000NK00K00OOOOOOO0Okxoooooooooooooooo
+** ccccccccccllollcccccccccccok'lo....   o.............cl  .o                     ;XOOOOOXO00KKKKKK0KKK0O0KK0Oxoooooooooooo
+** ccccccclloooooollccccccccccko..d'...  l............;x   l'               .   .cXOOOOO0NOkOOOOOOKK0OOOOOOOOOKKOdooooooooo
+** cccccllooooooooooolccccccccl0K:.d, ...:;..........,k   ,o                .:clcX0000000NkOO0OO0XOOOOO000O00OO00X0dooooooo
+** cclloooooooooooooooolodxkkOKN0Kx.dl:'..o.........,x   .k             .::c:. 'K0OOOOOOKK0OOK0KXkOOO00X00OOK0O0000X0oooool
+** llooooooooooodkOO0XKKKK0KKK0NK0Xd'dO, .o,.......;k    x.',.     .,:cc,.    .K0OOOOOOONOKOOXKNOkO0000X00OOOKOO0000KXdolcc
+** ooooooooook0XK0OKXKKKKKK0OOOKK00KOX0Xk:.x'.....cd   .'x.o:..,::l:.   ..   .K00000000XKOKOOKN0OOkOK00X000OOKOO000000Xoccc
+** oooooooo0XK000OXKK00K00KO0OOOX00000000kloKxc'.dc      o;;::;......       'K0OOOOOOO0NOOXOOKKO0OkOX00KX0OOOKOOO0K000OKlcc
+** ooooooxXKK000OXKOK00K0X0O00OOXK0000000O.cox;coxl,. ..    .           ...:XOOOOOOOOOX00X0OOXOkKkO0X000KXOOOXOO00K0000O0cc
+** oooooOX0K0KKOKXOOKK0X0K0OXO0O0X0000000Kood;.   .oOo'..       .;......  lX00OOOOOOOXK0X0OOOXkkKk0KX000O0KOOOKK00XK000K0xc
+** ooookXO0K0KK0NOOKK0X00K0K0OKOOXK000000Kd .:c:'.llOlo         c ......,KKOO0000000XXOX0OOO0KkkK000KX00OKKOOOO0K00KK00K0Kc
+** ooodXO0KK0X0KKOK000X0KKO0KO0000NK000000K...'.oxc,, ';        l      ,K0OOOOOOOOOKKkOXOOOOX0kk0K000KKOOK0OOOOOK0O0X00KX0x
+** oooK0KK00XKONOOK00XK0KO0XOOXOK00X0000000d '..'.c .  l        l     dX0OOOOOOOOOX0kKKKO0KKX0kkK00000X0OKKOOOOOOXOOK000KK0
+** ooxXOX00X0OONOK000X00KOKOOKKO0KO0X000000K,..   :.';;o        o   '00O00000000KXOOXOOOOX0OK0kk000000X0OOXOOOOOOX0O0X0000K
+** ooK0OX00K0O0XOK00XK0OKOK0O0KOO0KO0NK0000KO..    doddd.       l .dX0OOOOOOOO0XKkOXOkkOXKOO0KkkOKO00KX00O0XOOOOOX0O00XKK0O
+** olXOOX00K0OKKOKOKX000KOXOOOXOOX0OOKX00000Kc .   c;. ,; .     ; 0KO0KOOOOOO0NOkk0KkkkX0OOOOKkkkXOO0X0000OXKOOOOX0O00000OO
+**    ┌ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ┐
+**
+**    │                                   (c) 2020 Daniel P. Shannon <daniel@daniel.sh>                                   │
+**                                     Licensed under the GNU General Public License v3.0
+**    │                                                                                                                   │
+**     ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─
+*/
