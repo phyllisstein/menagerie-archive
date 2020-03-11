df1550d9953ffb7415713e9006dcbf94d41f3919
Daniel Shannon <daniel@daniel.sh>
2020-03-11 08:21:27 -0400

    :building_construction: Refactor top-level app.

---
+++ src/app/routes/app/app.tsx
@@ -0,0 +1,61 @@
+import { Redirect, Route, Routes } from 'react-router-dom'
+import avatarFB from 'assets/avatar-fb.jpg'
+import avatarTW from 'assets/avatar-tw.jpg'
+import { Body } from 'app/styles/global'
+import faviconPNG from 'assets/favicon.png'
+import { FunctionComponent } from 'react'
+import { Helmet } from 'react-helmet-async'
+import { Style } from '../style'
+import theme from 'app/styles/theme'
+import { ThemeProvider } from 'styled-components'
+
+const SITE_NAME = 'An Evening With Daniel P. Shannon'
+
+const DESCRIPTION = 'Talks and performances.'
+
+const KEYWORDS = [
+  'talks',
+  'tech talks',
+]
+
+export const App: FunctionComponent = () => {
+  return (
+    <ThemeProvider theme={ theme }>
+      <>
+        <Body />
+        <Helmet defaultTitle={ SITE_NAME } titleTemplate={ `%s | ${ SITE_NAME }` }>
+          <html lang='en' />
+
+          <meta charSet='utf-8' />
+          <meta content='width=device-width, initial-scale=1, shrink-to-fit=no' name='viewport' />
+
+          { /* Common Metadata */ }
+          <meta content={ DESCRIPTION } name='description' />
+          <meta content={ KEYWORDS.join(',') } name='keywords' />
+
+          { /* Facebook OpenGraph */ }
+          <meta content={ DESCRIPTION } property='og:description' />
+          <meta content={ avatarFB } property='og:image' />
+          <meta content='en_US' property='og:locale' />
+          <meta content={ SITE_NAME } property='og:site_name' />
+          <meta content='website' property='og:type' />
+
+          { /* Twitter Cards */ }
+          <meta content='summary' name='twitter:card' />
+          <meta content={ DESCRIPTION } property='twitter:description' />
+          <meta content='on' name='twitter:dnt' />
+          <meta content={ avatarTW } property='twitter:image' />
+          <meta content='@phyllisstein' name='twitter:site' />
+          <meta content={ SITE_NAME } property='twitter:title' />
+
+          { /* Favicon */ }
+          <link href={ faviconPNG } rel='icon' sizes='192x192' />
+        </Helmet>
+        <Routes>
+          <Redirect from='style' to='style/1' />
+          <Route element={ <Style /> } path='style/:step' />
+        </Routes>
+      </>
+    </ThemeProvider>
+  )
+}
--- src/app/routes/router.tsx
+++
@@ -1,8 +0,0 @@
-import { Route, Routes } from 'react-router-dom'
-import { FunctionComponent } from 'react'
-import { Style } from './style'
-
-export const Router: FunctionComponent = () =>
-  <Routes>
-    <Route element={ <Style /> } path='style/*' />
-  </Routes>
