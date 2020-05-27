e33af24dc10e10c71bc59971feafc4aa0bf289df
Daniel P. Shannon <daniel@fleet.i.ng>
2020-05-27 01:15:49 -0400

    Squashed commit of the following:commit b9cf89112ee6bf06f5a6e3d5165668661477b078Author: Daniel P. Shannon <daniel@daniel.sh>Date:   Wed May 27 01:10:03 2020 -0400    Back to TypeScript.

---
+++ src/app/styles/global/body.ts
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const Body = createGlobalStyle`
+  *,
+  *::before,
+  *::after {
+    box-sizing: inherit;
+
+    outline: none;
+    -webkit-tap-highlight-color: rgba(255, 255, 255, 0);
+  }
+
+  html {
+    box-sizing: border-box;
+
+    color: ${ ({ theme }) => theme.palette.css.text };
+    font: 65%/1.5 sans-serif;
+    font-feature-settings: 'kern', 'liga';
+    font-kerning: normal;
+    font-variant-ligatures: common-ligatures;
+  }
+
+  body {
+    ${ ({ theme }) => theme.typeface.primary() }
+
+    color: ${ ({ theme }) => theme.palette.css.text };
+  }
+`
