12aa507830178e342fd5e906c19b0267275e5bae
Daniel P. Shannon <daniel@daniel.sh>
2020-05-27 00:23:01 -0400

    Switch to React Loadable.

--- src/app/routes/app/index.js
+++ src/app/routes/app/index.js
@@ -1,1 +1,1 @@
-export { App } from './app'
+export { App as default } from './app'
--- src/client/bootstrap.js
+++ src/client/bootstrap.js
@@ -8,26 +8,11 @@ const loadCSS = async () => {
     ])
 }
 
-const loadGSAP = async () => {
-    const {gsap} = await import('gsap')
-
-    // Complex: 375ms
-    // Entering: 225ms
-    // Exiting: 195ms
-    gsap.registerEase('material-standard', gsap.parseEase('.4,0,.2,1'))
-    gsap.registerEase('material-deceleration', gsap.parseEase('0,0,.2,.1'))
-    gsap.registerEase('material-acceleration', gsap.parseEase('.4,0,1,1'))
-    gsap.registerEase('material-sharp', gsap.parseEase('.4,0,.6,1'))
-    gsap.registerEase('material-swift-ease-in-out', gsap.parseEase('.35,0,.25,1')) // 500ms
-    gsap.registerEase('material-swift-ease-out', gsap.parseEase('.25,.8,.25,1')) // 400ms
-    gsap.registerEase('material-swift-ease-in', gsap.parseEase('.55,0,.55,.2')) // 300ms
-}
-
 const renderApp = async () => {
     const [
-        {App},
-        {BrowserRouter},
-        {HelmetProvider},
+        { default: App },
+        { BrowserRouter },
+        { HelmetProvider },
     ] = await Promise.all([
         import('app'),
         import('react-router-dom'),
@@ -47,7 +32,7 @@ const renderApp = async () => {
 
     if (module.hot) {
         module.hot.accept('app', () => {
-            const {App} = require('app')
+            const { App } = require('app')
 
             reactRoot.render(
                 <BrowserRouter>
@@ -60,6 +45,6 @@ const renderApp = async () => {
     }
 }
 
-const bootstrap = () => Promise.all([loadCSS(), loadGSAP(), renderApp()])
+const bootstrap = () => Promise.all([loadCSS(), renderApp()])
 
 export default bootstrap
