4623f3624dacb8d11570b4c22f8052397b3b6625
Daniel P. Shannon <daniel@fleet.i.ng>
2021-02-21 12:14:09 -0500

    💄 Give steps a fixed size.

--- src/app/routes/approval-matrix/approval-matrix.js
+++ src/app/routes/approval-matrix/approval-matrix.js
@@ -1,26 +1,39 @@
 import { Controls, Impress, Layout, Step } from 'app/components/impress'
+import { StepWrapper } from './approval-matrix-styles'
 
 export const ApprovalMatrix = () => {
     return (
         <>
             <Impress height={ 768 } width={ 768 }>
-                <Layout>
-                    <div
-                        style={{
-                            backgroundColor: '#000',
-                            display: 'block',
-                            height: 2,
-                            width: 768,
-                        }} />
-                </Layout>
-                <Step position={{ z: 1024 }} scale={ 2 }>
-                    <h1>One (Step)</h1>
+                <Step>
+                    <StepWrapper>
+                        <h1>One</h1>
+                    </StepWrapper>
                 </Step>
-                <Layout>
-                    <h1>Two (Layout)</h1>
-                </Layout>
-                <Step position={{ x: 1024 }}>
-                    <h1>Two (Step)</h1>
+                <Step position={{ x: 1000 }}>
+                    <StepWrapper>
+                        <h1>Two</h1>
+                    </StepWrapper>
+                </Step>
+                <Step relative position={{ x: 1000 }}>
+                    <StepWrapper>
+                        <h1>Three</h1>
+                    </StepWrapper>
+                </Step>
+                <Step relative position={{ x: -500 }} scale={ 2 }>
+                    <StepWrapper>
+                        <h1>Four</h1>
+                    </StepWrapper>
+                </Step>
+                <Step rotation={{ z: 45 }}>
+                    <StepWrapper>
+                        <h1>Five</h1>
+                    </StepWrapper>
+                </Step>
+                <Step rotation={{ y: 180 }}>
+                    <StepWrapper>
+                        <h1>Six</h1>
+                    </StepWrapper>
                 </Step>
             </Impress>
             <Controls />
