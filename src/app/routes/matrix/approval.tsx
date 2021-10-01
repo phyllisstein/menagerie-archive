a6bf03188f04579bfbabcce4f4cd22725356cb0c
Daniel P. Shannon <daniel@fleet.i.ng>
2021-10-01 04:45:21 -0400

    🚧 WIP clipPath.

--- src/app/routes/matrix/approval.tsx
+++ src/app/routes/matrix/approval.tsx
@@ -1,29 +1,30 @@
 /* eslint-disable react/jsx-sort-props */
 
 import { Entry, Grid, Image, Line } from './approval-styles'
-import { ReactElement, useEffect, useState } from 'react'
+import { ReactElement, useCallback, useState } from 'react'
 import { Scene, Stage } from 'app/components'
 import madge from 'assets/matrix/madge.png'
 import Madge from 'assets/matrix/madge.svg'
 import Malcolm from 'assets/matrix/MalcolmGladwell.svg'
 import malcolmGladwell from 'assets/matrix/MalcolmGladwell.png'
+import R from 'ramda'
 
 export function ApprovalRoute (): ReactElement {
   const [step, setStep] = useState(0)
 
-  useEffect(() => {
-    const t = setTimeout(() => {
-      setStep(s => {
-        if (s === 4) {
-          return 0
+  const toggleCurrentStep = useCallback(
+    clickableStep => {
+      return () => {
+        if (clickableStep === step) {
+          setStep(0)
+          return
         }
 
-        return s + 1
-      })
-    }, 5000)
-
-    return () => clearTimeout(t)
-  })
+        setStep(clickableStep)
+      }
+    },
+    [step],
+  )
 
   return (
     <>
@@ -34,25 +35,32 @@ export function ApprovalRoute (): ReactElement {
             <Line $axis='y' />
           </Grid>
         </Scene>
-        <Scene translateX={ -512 }>
+        <Scene scale={ 2 } />
+        <Scene translateX={ -512 } onClick={ toggleCurrentStep(1) }>
           <Entry>
             <Entry.Text>
               <strong>Ponchos.</strong>
             </Entry.Text>
           </Entry>
         </Scene>
-        <Scene translateX={ -128 } translateY={ -256 }>
+        <Scene
+          style={{ pointerEvents: 'none' }}
+          translateX={ -128 }
+          translateY={ -256 }
+          onClick={ toggleCurrentStep(2) }>
           <Entry>
             <Entry.Text>
               <strong>Madonna</strong>, Oxford student.
             </Entry.Text>
-            <Image
-              src={ madge }
-              style={{ clipPath: "url('#madge_svg__madge')" }} />
+            <Image src={ madge } style={{ clipPath: "url('#madge')" }} />
             <Madge />
           </Entry>
         </Scene>
-        <Scene translateX={ -128 } translateY={ -512 } scale={ 0.5 }>
+        <Scene
+          translateX={ -128 }
+          translateY={ -512 }
+          scale={ 0.5 }
+          onClick={ toggleCurrentStep(3) }>
           <Entry style={{ minWidth: 256 }}>
             <Entry.Text>
               <strong>PBS Broadway-musical documentary:</strong> Noble but
@@ -60,7 +68,10 @@ export function ApprovalRoute (): ReactElement {
             </Entry.Text>
           </Entry>
         </Scene>
-        <Scene translateX={ -512 } translateY={ -256 }>
+        <Scene
+          translateX={ -512 }
+          translateY={ -256 }
+          onClick={ toggleCurrentStep(4) }>
           <Entry>
             <Entry.Text>
               <strong>
@@ -70,7 +81,7 @@ export function ApprovalRoute (): ReactElement {
             </Entry.Text>
             <Image
               src={ malcolmGladwell }
-              style={{ clipPath: "url('#MalcolmGladwell_svg__gladwell')" }} />
+              style={{ clipPath: "url('#gladwell')" }} />
             <Malcolm />
           </Entry>
         </Scene>
