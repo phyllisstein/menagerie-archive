a0dd4885eafdc7107e190baf81f445bd32078a06
Daniel P. Shannon <daniel@fleet.i.ng>
2021-10-17 06:34:02 -0400

    💄 Remove backface styles.

--- src/app/components/stage/scene-styles.ts
+++ src/app/components/stage/scene-styles.ts
@@ -2,13 +2,5 @@ import styled from 'styled-components'
 
 export const Root = styled.div`
   position: absolute;
-
   transform-style: preserve-3d;
-  backface-visibility: visible;
-
-  *,
-  *::before,
-  *::after {
-    backface-visibility: visible;
-  }
 `
