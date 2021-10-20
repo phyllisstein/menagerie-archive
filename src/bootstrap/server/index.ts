7a96f5abff5f30b163bc0699a29a2c614163e4eb
Daniel P. Shannon <daniel@fleet.i.ng>
2021-10-20 05:58:16 -0400

    📝 Update project name and mascot.

--- src/bootstrap/server/index.ts
+++ src/bootstrap/server/index.ts
@@ -1,20 +1,20 @@
 /**
  * Copyright (C) 2021 Daniel P. Shannon (<daniel@fleet.i.ng>)
  *
- * This file is part of Try-Works.
+ * This file is part of a Menagerie.
  *
- * Try-Works is free software: you can redistribute it and/or modify
+ * The Menagerie is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Try-Works is distributed in the hope that it will be useful,
+ * The Menagerie is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Try-Works.  If not, see <http://www.gnu.org/licenses/>.
+ * along with the Menagerie.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 import 'core-js'
