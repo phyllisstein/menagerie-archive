3a7bf80a5299cc2ff6b2342bcc156f1e2e5ea4c2
Daniel P. Shannon <daniel@fleet.i.ng>
2021-11-12 02:34:19 -0500

    🚧 WIP: Start azinc.

--- bin/refresh.mjs
+++ bin/refresh.mjs
@@ -9,32 +9,79 @@ import sharp from 'sharp'
 
 // sharp.concurrency(1)
 
+const IN_SITU = {
+  GREEN: chroma.rgb(106, 112, 75),
+  ORANGE: chroma.rgb(133, 107, 75),
+  YELLOW: chroma.rgb(134, 117, 73),
+}
+
+const AZINC = {
+  GREEN: chroma.rgb(125, 148, 73),
+  ORANGE: chroma.rgb(172, 132, 70),
+  YELLOW: chroma.rgb(176, 159, 70),
+}
+
+const DISTANCE = {
+  GREEN: chroma.distance(IN_SITU.GREEN, AZINC.GREEN, 'lab'),
+  ORANGE: chroma.distance(IN_SITU.ORANGE, AZINC.ORANGE, 'lab'),
+  YELLOW: chroma.distance(IN_SITU.YELLOW, AZINC.YELLOW, 'lab'),
+}
+
 export function refreshLightness (L) {
   const unscaled = R.clamp(
     0,
     255,
-    Math.floor(
-      255 * (0.834 * (L / 100) ** 2 + 0.309 * (L / 100) + 0.145)
-    ),
+    255 * (0.834 * (L / 100) ** 2 + 0.309 * (L / 100) + 0.145),
   )
 
-  return Math.floor(unscaled / 255 * 100)
+  return unscaled / 255 * 100
 }
 
 export function refreshA (a) {
   const unscaled = R.clamp(
     0,
     255,
-    Math.floor(
-      255 * (1.077 * ((a + 128) / 255) - 0.039),
-    )
+    255 * (1.077 * ((a + 128) / 255) - 0.039),
   )
   return unscaled - 128
 }
 
 export function refreshB (b) {
-  const unscaled = R.clamp(0, 255, Math.floor(255 * (1.09 * ((b + 128) / 255) - 0.054)))
+  const unscaled = R.clamp(0, 255, 255 * (1.09 * ((b + 128) / 255) - 0.054))
   return unscaled - 128
+}
+
+function azinc(lab) {
+  const inSitu = chroma.lab(lab)
+
+  const color = Object.entries(IN_SITU).sort(([name1, color1], [name2, color2]) => {
+    const distance1 = chroma.distance(inSitu, color1, 'lab')
+    const distance2 = chroma.distance(inSitu, color2, 'lab')
+    return distance1 - distance2
+  })
+}
+
+export function refresh (image) {
+  for (let byte = 0; byte < image.length; byte += 3) {
+    let red = image.readUint8(byte)
+    let green = image.readUint8(byte + 1)
+    let blue = image.readUint8(byte + 2)
+    let lab = chroma(red, green, blue, 'rgb').lab()
+    lab[0] = refreshLightness(lab[0])
+    lab[1] = refreshA(lab[1])
+    lab[2] = refreshB(lab[2])
+    lab = azinc(lab)
+    let newRGB = chroma(L, a, b, 'lab').rgb()
+    image.writeUint8(newRGB[0], byte)
+    image.writeUint8(newRGB[1], byte + 1)
+    image.writeUint8(newRGB[2], byte + 2)
+  }
+
+  return image
+}
+
+export function azinc (image) {
+
 }
 
 export async function processImage (imageFileName) {
@@ -64,7 +111,7 @@ export async function processImage (imageFileName) {
   }
 }
 
-const images = await globby('./src/assets/la-grande-jatte/*-15.jpg')
+const images = await globby(process.argv.slice(2))
 const queue = new PQueue({ concurrency: 3 })
 
 for await (const image of images) {
