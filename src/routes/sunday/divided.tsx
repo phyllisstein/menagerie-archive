ff82ac8b5073f5a64c36acb317cb33909151485d
Daniel P. Shannon <daniel@fleet.i.ng>
2022-02-04 02:57:29 -0500

    🚧 Next class-based Packer draft.

--- src/routes/sunday/divided.tsx
+++ src/routes/sunday/divided.tsx
@@ -1,35 +1,32 @@
 import _ from 'lodash'
-import { FunctionComponent, useMemo } from 'react'
+import { FunctionComponent, useEffect, useMemo, useRef } from 'react'
 
 import { Bar, Circle, Ellipse, Root, SVG } from './colors-styles'
+import { Packer, nodeTestFunction } from './packer'
+import { COLORS_RGB } from './palette'
 
 const getGridItems = (elementCount: number) => []
 
 export const Divided: FunctionComponent = () => {
-  const orangeGridItems = useMemo(() => getGridItems(128), [])
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+
+  useEffect(() => {
+    const canvas = canvasRef.current?.getContext('2d')
+    if (!canvas) return
+
+    const rootNode = nodeTestFunction()
+    for (const block of rootNode) {
+      const color = _.sample(Object.entries(COLORS_RGB))
+      canvas.fillStyle = color![1]
+      console.log({ block, color: color![0] })
+      canvas.fillRect(block.x, block.y, block.width, block.height)
+    }
+  }, [])
 
   return (
     <>
       <Root>
-        <Bar>
-          <SVG height='256' viewBox='0 0 512 256' width='512'>
-            { orangeGridItems.map(item => {
-              const color = _.sample(['BLUE', 'GREEN', 'ORANGE', 'RED']) as
-                | 'BLUE'
-                | 'GREEN'
-                | 'ORANGE'
-                | 'RED'
-              return (
-                <Circle
-                  key={ `${ item.x }-${ item.y }` }
-                  $color={ color }
-                  cx={ item.x }
-                  cy={ item.y }
-                  r={ item.width / 2 } />
-              )
-            }) }
-          </SVG>
-        </Bar>
+        <canvas ref={ canvasRef } />
       </Root>
     </>
   )
--- src/routes/sunday/packer.ts
+++ src/routes/sunday/packer.ts
@@ -3,21 +3,28 @@
  * cached and retried at each iteration.
  */
 export class Packer {
-  private box = new Node()
+  private box: Node
 
-  constructor (private maxWidth: number, private maxHeight: number) {}
+  static randomBin (maxWidth: number, maxHeight: number): Node {
+    const randomPacker = new Packer(maxWidth, maxHeight)
 
-  private findFreeSpace (node: Node, width: number, height: number): Node {
-    if (node.used) {
-      return (
-        this.findFreeSpace(node.right, width, height) ||
-        this.findFreeSpace(node.down, width, height)
-      )
+    randomPacker.packNode(new Node(Math.floor(500), Math.floor(200)))
+
+    randomPacker.packNode(new Node(Math.floor(250), Math.floor(200)))
+
+    for (let i = 0; i < 20; i++) {
+      randomPacker.packNode(new Node(Math.floor(50), Math.floor(50)))
     }
 
-    if (width <= node.width && height <= node.height) {
-      return node
-    }
+    return randomPacker.box
+  }
+
+  constructor (private maxWidth: number, private maxHeight: number) {
+    this.box = new Node(0, 0)
+  }
+
+  packNode (node: Node) {
+    this.box.packChild(node)
   }
 }
 
@@ -31,6 +38,18 @@ export class Node {
     public x: number = 0,
     public y: number = 0,
   ) {}
+
+  * [Symbol.iterator] () {
+    yield this
+
+    if (this.right) {
+      yield* this.right
+    }
+
+    if (this.down) {
+      yield* this.down
+    }
+  }
 
   get empty (): boolean {
     return this.down === undefined && this.right === undefined
@@ -47,82 +66,83 @@ export class Node {
     return nextNode
   }
 
-  packChild (node: Node): Node {
-    if (this.empty && node.width <= this.width && node.height <= this.height) {
-      return this
-    }
-  }
-
-  fit (node: Node): Node | null {
-    if (this.right?.fitChild(node)) {
-      return this.right.split(node)
+  packChild (child: Node): Node {
+    if (!this.empty) {
+      return this.right.packChild(child) || this.down.packChild(child)
     }
 
-    if (this.down?.fit(node)) {
-      return this.down
+    if (child.width <= this.width && child.height <= this.height) {
+      return this.split(child)
     }
 
-    return this.split(node.width, node.height)
+    return this.growToFit(child)
   }
 
-  private fitChild (node: Node): boolean {
-    return node.width <= this.width && node.height <= this.height
-  }
-
-  growToFit (node: Node): Node {
-    const canGrowDown = node.width <= this.width
-    const canGrowRight = node.height <= this.height
+  growToFit (child: Node): Node {
+    const canGrowDown = child.width <= this.width
+    const canGrowRight = child.height <= this.height
 
     const shouldGrowRight =
-      canGrowRight && this.height >= this.width + node.width
+      canGrowRight && this.height >= this.width + child.width
     const shouldGrowDown =
-      canGrowDown && this.width >= this.height + node.height
+      canGrowDown && this.width >= this.height + child.height
 
     if (shouldGrowRight) {
-      return this.growRight(width, height)
+      return this.growRight(child)
     }
 
     if (shouldGrowDown) {
-      return this.growDown(width, height)
+      return this.growDown(child)
     }
 
     if (canGrowRight) {
-      return this.growRight(width, height)
+      return this.growRight(child)
     }
 
     if (canGrowDown) {
-      return this.growDown(width, height)
+      return this.growDown(child)
     }
 
     return this
   }
 
-  private growDown (node: Node): Node {
-    this.down = new Node(0, this.height, this.width, node.height)
+  private growDown (child: Node): Node {
+    this.down = new Node(0, this.height, this.width, child.height)
     this.right = this.copy()
 
     this.x = 0
     this.y = 0
-    this.height = this.height + node.height
+    this.height = this.height + child.height
 
-    return this.split()
+    return this.split(child)
   }
 
-  private growRight (node: Node): Node {
-    this.right = new Node(this.width, 0, width, this.height)
+  private growRight (child: Node): Node {
+    this.right = new Node(this.width, 0, child.width, this.height)
     this.down = this.copy()
-    return this.split(width, height)
+    return this.split(child)
   }
 
-  private split (width: number, height: number): Node {
-    this.right = new Node(this.x + width, this.y, this.width - width, height)
+  private split (child: Node): Node {
+    this.right = new Node(
+      this.width - child.width,
+      child.height,
+      this.x + child.width,
+      this.y,
+    )
     this.down = new Node(
-      this.x,
-      this.y + height,
       this.width,
-      this.height - height,
+      this.height - child.height,
+      this.x,
+      this.y + child.height,
     )
 
     return this
   }
+}
+
+export function nodeTestFunction () {
+  const rootNode = Packer.randomBin(500, 500)
+  const [_, nodes] = [...rootNode]
+  return nodes
 }
